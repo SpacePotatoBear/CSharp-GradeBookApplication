f84954a45a02fb4d0db0a7403418b8b8cfa4024b
Brandon Hope <bhope@beyondtrust.com>
2021-11-16 16:50:11 -0400

    b

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,19 +12,19 @@ namespace GradeBook.GradeBooks
         	if (Students.Count < 5) {
         		throw new System.InvalidOperationException("Not enough students");
         	}
+        	var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
         	Students.Sort(delegate(Student A, Student B) {
         		if(A.AverageGrade > B.AverageGrade) return 1;
         		if(A.AverageGrade == B.AverageGrade) return 0;
         		else return -1;
         	});
-
         	// get the index of the first member larger than us
         	int i;
-        	for (i = 0; i < Students.Count; i++) {
-        		if (averageGrade < Students[i].AverageGrade) break;
+        	for (i = 0; i < grades.Count; i++) {
+        		if (averageGrade < grades[i].AverageGrade) break;
         	}
         	// + 1 to account for zero indexing
-        	double gradePercentile = (i) / (Students.Count);
+        	double gradePercentile = (i+ 1) / (grades.Count + 1);
         	if(gradePercentile >= 0.8) return 'A';
         	else if(gradePercentile >= 0.6) return 'B';
         	else if(gradePercentile >= 0.4) return 'C';
