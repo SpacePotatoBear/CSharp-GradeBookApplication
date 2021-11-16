263892b4a37b3b2c9479b9327a131a57a2da30a0
Brandon Hope <bhope@beyondtrust.com>
2021-11-16 17:23:22 -0400

    fixed integer casting breaking the percentile calculation

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 using GradeBook.Enums;
-
+using System;
+using System.Linq;
 namespace GradeBook.GradeBooks
 {
 	public class RankedGradeBook : BaseGradeBook 
@@ -12,23 +13,25 @@ namespace GradeBook.GradeBooks
         	if (Students.Count < 5) {
         		throw new System.InvalidOperationException("Not enough students");
         	}
-        	var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+        	//var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
         	Students.Sort(delegate(Student A, Student B) {
-        		if(A.AverageGrade > B.AverageGrade) return 1;
+                // Sorts descending
+        		if(A.AverageGrade > B.AverageGrade) return -1;
         		if(A.AverageGrade == B.AverageGrade) return 0;
-        		else return -1;
+        		else return 1;
         	});
-        	// get the index of the first member larger than us
+        	// get the index of the first member smaller than us
         	int i;
-        	for (i = 0; i < grades.Count; i++) {
-        		if (averageGrade < grades[i].AverageGrade) break;
+        	for(i = 0; i < Students.Count; i++) {
+                if (averageGrade >= Students[i].AverageGrade) break;
         	}
         	// + 1 to account for zero indexing
-        	double gradePercentile = (i+ 1) / (grades.Count + 1);
-        	if(gradePercentile >= 0.8) return 'A';
-        	else if(gradePercentile >= 0.6) return 'B';
-        	else if(gradePercentile >= 0.4) return 'C';
-        	else if(gradePercentile >= 0.2) return 'B';
+        	double gradePercentile = (double)(i + 1) / (double)Students.Count;
+
+            if (gradePercentile <= 0.2) return 'A';
+        	else if(gradePercentile <= 0.4) return 'B';
+        	else if(gradePercentile <= 0.6) return 'C';
+        	else if(gradePercentile <= 0.8) return 'D';
         	else return 'F';
         }
 	}
